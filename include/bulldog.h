00d2fea3e5d9f31b850499114380c767d10a4e02
Martin Blasko <mblasko42@gmail.com>
2015-08-23 01:47:07 +0200

    tweaks

---
+++ include/bulldog.h
@@ -0,0 +1,42 @@
+#include "cinder/Filesystem.h"
+using namespace std;
+using namespace ci;
+
+#pragma once
+
+struct wFile
+{
+	time_t time;
+	fs::path path;
+	std::function<void()> callback;
+	wFile(time_t t, fs::path p, std::function<void()> c) {
+		time = t;
+		path = p;
+		callback = c;
+	}
+};
+class BullDog
+{
+public:
+	BullDog(){};
+	~BullDog(){};
+	void watch(fs::path p, std::function<void()> callback) {
+		if(fs::exists(p))list.emplace_back(last_write_time(p), p, callback);
+	}
+
+	void check(){
+		list.erase(remove_if(list.begin(), list.end(), [&](wFile w){ return !fs::exists(w.path);}), list.end());
+		for (auto &item : list){
+			auto lw = last_write_time(item.path);
+
+			if (lw > 0 && lw != item.time){
+				item.callback();
+				item.time = lw;
+			}	
+		}
+	}
+private:
+	vector<wFile> list;
+};
+
+
